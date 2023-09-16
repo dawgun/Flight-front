75b83957a8409df357275fddd473f337c6203072
David <dawgun@hotmail.com>
2023-09-16 14:14:28 +0200

    feat: Install vitest and testing library for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./jest-setup.ts",
+    mockReset: true,
+    include: ["src/**/*.test.tsx", "src/**/*.test.ts"],
+  },
 });
