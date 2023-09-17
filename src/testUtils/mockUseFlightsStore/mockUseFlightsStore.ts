d510d527fbb3657f6af4e65af2ec32301184c920
David <dawgun@hotmail.com>
2023-09-17 14:00:56 +0200

    feat: Create mock flights store for testing

---
+++ src/testUtils/mockUseFlightsStore/mockUseFlightsStore.ts
@@ -0,0 +1,22 @@
+import {
+  Flight,
+  FlightStore,
+} from "../../store/useFlightsStore/types/flightTypes";
+import useFlightsStore from "../../store/useFlightsStore/useFlightsStore";
+import { mockFlight, mockFlights } from "../mockFlight/mockFlight";
+
+const mockStore = (flights: Flight[], selectedFlight: Flight): FlightStore => ({
+  flights: flights,
+  selectedFlight: selectedFlight,
+  loadFlights: vi.fn(),
+});
+
+const originalStore = useFlightsStore.getState();
+const listOfFlights = mockFlights();
+const flight = mockFlight();
+
+export const resetFlightStore = () => useFlightsStore.setState(originalStore);
+export const mockFlightStore = (
+  flights = listOfFlights,
+  selectedFlight = flight
+) => useFlightsStore.setState(mockStore(flights, selectedFlight));
