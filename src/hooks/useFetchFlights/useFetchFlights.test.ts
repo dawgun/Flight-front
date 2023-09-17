e20ef7a46771e6ec193da1fdc7f75ec3bcb63624
David <dawgun@hotmail.com>
2023-09-17 14:03:28 +0200

    feat: Create test for fetch flights hook

---
+++ src/hooks/useFetchFlights/useFetchFlights.test.ts
@@ -0,0 +1,55 @@
+import "../../testUtils/mockReactTostify/mockReactTostify";
+import { mockFlightStore } from "../../testUtils/mockUseFlightsStore/mockUseFlightsStore";
+import { renderHook } from "@testing-library/react";
+import useFetchFlights from "./useFetchFlights.js";
+import useFlightsStore from "../../store/useFlightsStore/useFlightsStore.js";
+import { Mock } from "vitest";
+import { toast } from "react-toastify";
+
+describe("GIVEN the useFetchFlights custom hook", () => {
+  describe("WHEN call the getFlights function with fetch error", () => {
+    test("THEN shouldn't call loadFlights function the list of flights received", async () => {
+      mockFlightStore();
+      const { loadFlights } = useFlightsStore.getState();
+
+      const { result } = renderHook(() => useFetchFlights());
+      await result.current.getFlights();
+
+      expect(loadFlights).not.toHaveBeenCalled();
+    });
+
+    test("THEN shouldn't call loadFlights function the list of flights received", async () => {
+      mockFlightStore();
+      const expectedToastError = "Error loading flights";
+
+      const { result } = renderHook(() => useFetchFlights());
+      await result.current.getFlights();
+
+      expect(toast.error).toHaveBeenCalledWith(expectedToastError);
+    });
+  });
+
+  describe("WHEN call the getFlights function with correct fetch", () => {
+    test("THEN should call loadFlights function the list of flights received", async () => {
+      mockFlightStore();
+      const { loadFlights } = useFlightsStore.getState();
+
+      const { result } = renderHook(() => useFetchFlights());
+      await result.current.getFlights();
+
+      const expectedFlights = (loadFlights as Mock).mock.calls[0][0];
+
+      expect(loadFlights).toHaveBeenCalledWith(expectedFlights);
+    });
+
+    test("THEN should call toast with 'Flights Loaded'", async () => {
+      mockFlightStore();
+      const expectedSuccessToast = "Flights Loaded";
+
+      const { result } = renderHook(() => useFetchFlights());
+      await result.current.getFlights();
+
+      expect(toast.success).toHaveBeenCalledWith(expectedSuccessToast);
+    });
+  });
+});
