3eab3566f6d6d217076abdaac84c51dfa830b462
David <dawgun@hotmail.com>
2023-09-20 14:44:36 +0200

    feat: Add startFlight function with eventSource in ServerStreamStore

--- src/store/useServerStreamStore/types/types.ts
+++ src/store/useServerStreamStore/types/types.ts
@@ -1,6 +1,8 @@
 import { LatLngTuple } from "leaflet";
 
 export interface ServerStreamStore {
+  eventSource: EventSource;
   planePosition: LatLngTuple;
   setPlanePosition: (newPlanePosition: LatLngTuple) => void;
+  startFlight: (eventSource: EventSource) => void;
 }
